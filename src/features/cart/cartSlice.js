276534d0c4ac768984546b6713e7f726489c186e
sumit <sumithjadhav99@gmail.com>
2023-07-08 18:55:22 +0530

    remove axios

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import cartItems from '../../cartItems'
 
 const url = 'https://course-api.com/react-useReducer-cart-project'
 
@@ -10,17 +10,11 @@ const initialState = {
   isLoading: true,
 }
 
-export const getCartItems = createAsyncThunk(
-  'cart/getCartItems',
-  async (name, thunkAPI) => {
-    try {
-      const resp = await axios(url)
-      return resp.data
-    } catch (error) {
-      return thunkAPI.rejectWithValue('something went wrong')
-    }
-  }
-)
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+  return fetch(url)
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err))
+})
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -57,12 +51,10 @@ const cartSlice = createSlice({
       state.isLoading = true
     },
     [getCartItems.fulfilled]: (state, action) => {
-      console.log(action)
       state.isLoading = false
       state.cartItems = action.payload
     },
-    [getCartItems.rejected]: (state, action) => {
-      console.log(action)
+    [getCartItems.rejected]: (state) => {
       state.isLoading = false
     },
   },
